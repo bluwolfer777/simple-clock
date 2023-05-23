5f613a4605bd113dcea4fce71d34b888d845173f
bluwolfer777 <72340837+bluwolfer777@users.noreply.github.com>
2023-05-23 12:15:49 +0200

    version 1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Esercizio_3._12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(146, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(81, 9);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(191, 9);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(258, 9);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(323, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 63);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ":";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(368, 9);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(438, 9);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(509, 130);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esercizio_3._12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime orario = DateTime.Now;
+            int hour = (int)orario.Hour;
+            int minutes = (int)orario.Minute;
+            int seconds = (int)orario.Second;
+            if (hour == 0)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("0.png");
+            }
+            else if (hour == 1)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("1.png");
+            }
+            else if (hour == 2)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("2.png");
+            }
+            else if (hour == 3)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("3.png");
+            }
+            else if (hour == 4)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("4.png");
+            }
+            else if (hour == 5)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("5.png");
+            }
+            else if (hour == 6)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("6.png");
+            }
+            else if (hour == 7)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("7.png");
+            }
+            else if (hour == 8)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("8.png");
+            }
+            else if (hour == 9)
+            {
+                pictureBox1.Image = Image.FromFile("0.png");
+                pictureBox2.Image = Image.FromFile("9.png");
+            }
+            else if (hour == 10)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("0.png");
+            }
+            else if (hour == 11)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("1.png");
+            }
+            else if (hour == 12)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("2.png");
+            }
+            else if (hour == 13)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("3.png");
+            }
+            else if (hour == 14)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("4.png");
+            }
+            else if (hour == 15)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("5.png");
+            }
+            else if (hour == 16)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("6.png");
+            }
+            else if (hour == 17)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("7.png");
+            }
+            else if (hour == 18)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("8.png");
+            }
+            else if (hour == 19)
+            {
+                pictureBox1.Image = Image.FromFile("1.png");
+                pictureBox2.Image = Image.FromFile("9.png");
+            }
+            else if (hour == 20)
+            {
+                pictureBox1.Image = Image.FromFile("2.png");
+                pictureBox2.Image = Image.FromFile("0.png");
+            }
+            else if (hour == 21)
+            {
+                pictureBox1.Image = Image.FromFile("2.png");
+                pictureBox2.Image = Image.FromFile("1.png");
+            }
+            else if (hour == 22)
+            {
+                pictureBox1.Image = Image.FromFile("2.png");
+                pictureBox2.Image = Image.FromFile("2.png");
+            }
+            else if (hour == 23)
+            {
+                pictureBox1.Image = Image.FromFile("2.png");
+                pictureBox2.Image = Image.FromFile("3.png");
+            }
+
+            if (minutes < 10)
+            {
+                pictureBox3.Image = Image.FromFile("0.png"); 
+            }
+            else if (minutes < 20)
+            {
+                pictureBox3.Image = Image.FromFile("1.png");
+                minutes -= 10;
+            }
+            else if (minutes < 30)
+            {
+                pictureBox3.Image = Image.FromFile("2.png");
+                minutes -= 20;
+            }
+            else if (minutes < 40)
+            {
+                pictureBox3.Image = Image.FromFile("3.png");
+                minutes -= 30;
+            }
+            else if (minutes < 50)
+            {
+                pictureBox3.Image = Image.FromFile("4.png");
+                minutes -= 40;
+            }
+            else
+            {
+                pictureBox3.Image = Image.FromFile("5.png");
+                minutes -= 50;
+            }
+
+            if (minutes == 0)
+            {
+                pictureBox4.Image = Image.FromFile("0.png");
+            }
+            else if (minutes == 1)
+            {
+                pictureBox4.Image = Image.FromFile("1.png");
+            }
+            else if (minutes == 2)
+            {
+                pictureBox4.Image = Image.FromFile("2.png");
+            }
+            else if (minutes == 3)
+            {
+                pictureBox4.Image = Image.FromFile("3.png");
+            }
+            else if (minutes == 4)
+            {
+                pictureBox4.Image = Image.FromFile("4.png");
+            }
+            else if (minutes == 5)
+            {
+                pictureBox4.Image = Image.FromFile("5.png");
+            }
+            else if (minutes == 6)
+            {
+                pictureBox4.Image = Image.FromFile("6.png");
+            }
+            else if (minutes == 7)
+            {
+                pictureBox4.Image = Image.FromFile("7.png");
+            }
+            else if (minutes == 8)
+            {
+                pictureBox4.Image = Image.FromFile("8.png");
+            }
+            else if (minutes == 9)
+            {
+                pictureBox4.Image = Image.FromFile("9.png");
+            }
+
+            if (seconds < 10)
+            {
+                pictureBox5.Image = Image.FromFile("0.png");
+            }
+            else if (seconds < 20)
+            {
+                pictureBox5.Image = Image.FromFile("1.png");
+                seconds -= 10;
+            }
+            else if (seconds < 30)
+            {
+                pictureBox5.Image = Image.FromFile("2.png");
+                seconds -= 20;
+            }
+            else if (seconds < 40)
+            {
+                pictureBox5.Image = Image.FromFile("3.png");
+                seconds -= 30;
+            }
+            else if (seconds < 50)
+            {
+                pictureBox5.Image = Image.FromFile("4.png");
+                seconds -= 40;
+            }
+            else
+            {
+                pictureBox5.Image = Image.FromFile("5.png");
+                seconds -= 50;
+            }
+
+            if (seconds == 0)
+            {
+                pictureBox6.Image = Image.FromFile("0.png");
+            }
+            else if (seconds == 1)
+            {
+                pictureBox6.Image = Image.FromFile("1.png");
+            }
+            else if (seconds == 2)
+            {
+                pictureBox6.Image = Image.FromFile("2.png");
+            }
+            else if (seconds == 3)
+            {
+                pictureBox6.Image = Image.FromFile("3.png");
+            }
+            else if (seconds == 4)
+            {
+                pictureBox6.Image = Image.FromFile("4.png");
+            }
+            else if (seconds == 5)
+            {
+                pictureBox6.Image = Image.FromFile("5.png");
+            }
+            else if (seconds == 6)
+            {
+                pictureBox6.Image = Image.FromFile("6.png");
+            }
+            else if (seconds == 7)
+            {
+                pictureBox6.Image = Image.FromFile("7.png");
+            }
+            else if (seconds == 8)
+            {
+                pictureBox6.Image = Image.FromFile("8.png");
+            }
+            else if (seconds == 9)
+            {
+                pictureBox6.Image = Image.FromFile("9.png");
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
